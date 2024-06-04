3daf31b0a8946979731c6c4a81dd05e94275b7b8
Fritz1304 <freetoplay1304@gmail.com>
2024-06-04 12:28:26 -0500

    Agregar código de Spring Boot

---
+++ demo/src/main/java/com/example/demo/controllers/UsuarioController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UsuarioController {
+
+    @RequestMapping(value = "prueba")
+   public List<String> prueba(){
+       return List.of("Peras","Maracuya","Fresas");
+   }
+
+
+}
