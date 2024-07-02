e9a451670b3b67e4a26674745c827a0ff2af615a
Fritz1304 <freetoplay1304@gmail.com>
2024-07-02 16:25:57 -0500

    agregando funcionalidad básica

---
+++ demo/src/main/java/com/example/demo/repository/ArticuloRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Articulo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+    @Repository
+    public interface ArticuloRepository extends JpaRepository<Articulo, Long> {
+        Optional<Articulo> findByNombre(String nombre);
+        boolean existsByNombre(String nombre);
+    }
+
+
